79b898568707c2f28e50ff732c8b75c114ef1ba9
Aaron Goshine <greenlig@gmail.com>
2017-06-26 12:40:07 +0100

    chore(Exercism): added Exercism

---
+++ go/hello-world/hello_world.go
@@ -0,0 +1,17 @@
+// This is a "stub" file.  It's a little start on your solution.
+// It's not a complete solution though; you have to write some code.
+
+// The package name is expected by the test program.
+// It has to stay just the way it is.
+package hello
+const testVersion = 2
+
+// It's good style to write a comment here documenting HelloWorld.
+// (But delete all these instructional comments!)
+func HelloWorld(string) string {
+  if (string) {
+    return "Hello, " + string + "!";
+  } else {
+    return "Hello, World!";
+  }
+}
